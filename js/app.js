7c989227b5c8e1086bdcd54680841c1159688d5c
gthatcher <37427394+gthatcher@users.noreply.github.com>
2018-03-24 14:13:43 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -7,9 +7,9 @@
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
         attribution: 'Developed by <a href="#" target="_blank">Jade Allen Cook</a> & <a href="#" target="_blank">Geoffery Thatcher</a> for <a href="#" target="_blank">Deseret News</a>',
         maxZoom: 18,
-        id: 'mapbox.streets',
+        id: 'mapbox.dark',
         accessToken: 'pk.eyJ1IjoiamFkZWFsbGVuY29vayIsImEiOiJjamU3cTA5ZDgwNTNnMnFvMWRjdnI1aXYzIn0.63_s4EQVQur7r670eBY3DA',
-        style: 'mapbox://styles/mapbox/light-v9'
+        style: 'mapbox://styles/mapbox/dark-v9'
     }).addTo(map);
 
     // add map to container
